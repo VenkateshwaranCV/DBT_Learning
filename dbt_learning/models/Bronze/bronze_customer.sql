2e837a00743732f2cfbdd040897ee19d8bb808e3
VenkateshwaranCV <cvvenkateshwaranofl@gmail.com>
2025-10-05 17:51:51 -0400

    Initial commit

---
+++ dbt_learning/models/Bronze/bronze_customer.sql
@@ -0,0 +1,3 @@
+Select * 
+From 
+    {{ source('source', 'dim_customer') }}
---
+++ dbt_learning/models/Bronze/bronze_sales.sql
@@ -0,0 +1,9 @@
+{{
+  config(
+    materialized = 'view'
+    )
+}}
+
+Select * 
+From 
+    {{ source('source', 'fact_sales') }}
